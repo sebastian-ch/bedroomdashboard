e0a5f5fa40db95ded69c2ebc0ad3c5f4613670a4
Sebastian Hancock <sebastian.hancock@tum.de>
2021-12-04 22:26:37 -0800

    change background color for sunrise

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {useEffect, useState} from 'react';
+
 import Time from './Time'
 import './App.css';
 import Sunrise from './Sunrise';
@@ -5,12 +7,49 @@ import Wiki from './Wiki';
 import Marvel from './Marvel';
 
 function App() {
+
+  const [bcolor, setBcolor] = useState('#030201');
+
+  useEffect(() => {
+
+    function setBackgroundColor() {
+
+      let t = new Date();
+      let hr = t.getHours();
+
+      if (hr === 5) {
+        setBcolor('#AC66CC')
+      } else if ( hr === 6) {
+        setBcolor('#F6B8B8')
+      } else if (hr === 7) {
+        setBcolor('#FFED99')
+      } else if (hr >= 8) {
+        setBcolor('#030201')
+      }
+
+    }
+
+    setBackgroundColor();
+
+    setInterval(() => {
+      setBackgroundColor();
+    }, 3600000)
+
+
+  },[])
+  
+  
   return (
-    <div className="App">
+    <div className="App"
+          style={{
+            backgroundColor: bcolor
+          }}
+    
+    >
      <Time />
      <Sunrise />
      <div className='rows'>
-     <Wiki />
+      <Wiki />
      <Marvel />
      </div>
     </div>
